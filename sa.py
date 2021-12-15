136e4930a0181e9f3f0fb0dab095d65ef0c6e1c6
Gideon <ilunggideon@gmail.com>
2021-12-15 08:50:13 +0200

    github update

--- sa.py
+++ sa.py
@@ -3,9 +3,9 @@ from aco import Ant
 model = Ant()
 file_link = 'Data/st70/st70.tsp'
 A = model.get_distance_matrix(file_link)
-intial_param = [0.5,1,1,100,35,1]  #[p,alpha,beta,n,k,Q]#
+intial_param = [0.5,1,1,500,35,1]  #[p,alpha,beta,n,k,Q]#
 
-param,cost,path = model.SA(A,intial_param,t0=1,t1=2,n=3,alpha=0.5,update_type='all',ratio = 0.3,tol = 0.001,rep=20)
+param,cost,path = model.SA(A,intial_param,t0=1000,t1=10000,n=100,alpha=0.5,update_type='all',ratio = 0.3,tol = 0.001,rep=20)
 
 print("Parameters : ",param,"\n \n","total distance traveled : {}".format(cost),"\n \n","Path taken : \n",path)
 
